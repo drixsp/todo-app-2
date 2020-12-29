2c4bd0e27900892b8d70e1fbb40bb796d7c49559
Drix Ponteres <ponteresdrix@gmail.com>
2020-12-30 01:41:48 +0800

    Added About page

--- todo-project/src/App.vue
+++ todo-project/src/App.vue
@@ -1,56 +1,30 @@
 <template>
   <div id="app">
-    <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <div id="nav">
+      <Header />
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos'
 import Header from './components/layout/Header'
-import AddTodo from './components/AddTodo'
-
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    Todos,
-    Header,
-    AddTodo
-  },
-  data(){
-    return{
-      todos: [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: false
-        },
-        {
-          id: 2,
-          title: 'Todo Two',
-          completed: true
-        },
-        {
-          id: 3,
-          title: 'Todo Three',
-          completed: false
-        }
-      ]
-    }
-  },
-  methods: {
-    deleteTodo(id){
-      this.todos = this.todos.filter(todo => todo.id !== id)
-    },
-    addTodo(newTodo){
-      this.todos = [...this.todos, newTodo];
-    }
+    Header
   }
 }
 </script>
-
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+}
+
 *{
   box-sizing: border-box;
   padding: 0;
@@ -62,16 +36,4 @@ body{
   line-height: 1.4;
 }
 
-.btn{
-  display: inline-block;
-  border: none;
-  background: #555;
-  color: #fff;
-  padding: 7px 20px;
-  cursor: pointer;
-}
-
-.btn:hover{
-  background: #666;
-}
 </style>
--- todo-project/src/components/layout/Header.vue
+++ todo-project/src/components/layout/Header.vue
@@ -1,25 +1,35 @@
 <template>
     <header class="header">
-        <h1>Todo List</h1>    
+        <h1>Todo List</h1>
+        <p>A sample todo list app using Vue.js framework.</p>
+        
+        <div class="btn-group">
+            <router-link id="header-link" class="btn btn-primary" to="/">Home</router-link>
+            <router-link id="header-link" class="btn btn-primary" to="/about">About</router-link>    
+        </div>
+        
     </header>    
 </template>
 
-<script>
-export default {
-    name: "Header"
-}
-</script>
 
 <style scoped>
+    *{
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
     .header{
-        background: #333;
+        background: #002261;
         color: #fff;
         text-align: center;
         padding: 10px;
+        height: 150px;
     }
-
     .header a{
         color: #fff;
-        padding-right: 5px;
+        /* padding-right: 5px; */
+    }
+    #header-link{
+        margin: auto;
     }
 </style>
